b122559dedaba10629d08d1953886ac500c4a0fe
ztzrk <ralif877@gmail.com>
2024-05-25 14:56:06 +0700

    feat: add male and female image constant

--- recything_application/lib/constants/image_constant.dart
+++ recything_application/lib/constants/image_constant.dart
@@ -1,4 +1,8 @@
 class ImageConstant {
   static const String medalEditProfil =
       'assets/images/edit_profile_images/medal.png';
+  static const String maleEditProfil =
+      'assets/images/edit_profile_images/male.png';
+  static const String femaleEditProfil =
+      'assets/images/edit_profile_images/female.png';
 }
