3c686d6809c69af1fbc9d59ec8907d879dd2dd87
Fadhl Al-Hafizh <fadhlhafizh18@students.unnes.ac.id>
2024-05-25 21:11:58 +0700

    feat(splash): complete slicing for splash screen

--- recything_application/lib/constants/image_constant.dart
+++ recything_application/lib/constants/image_constant.dart
@@ -1,3 +1,10 @@
 class ImageConstant {
-  
+  //splash screen
+  static const String logoShape ='assets/images/splash/logo_shape_recything.png';
+  static const String logoText = 'assets/images/splash/logo_text_recything.png';
+  static const String logo = 'assets/images/splash/logo_complete_recything.png';
+
+  //onboarding screen
+  //welcome
+  //auth
 }
--- recything_application/lib/screens/splash/splash_screen.dart
+++ recything_application/lib/screens/splash/splash_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:recything_application/constants/color_constant.dart';
+import 'package:recything_application/constants/image_constant.dart';
+import 'package:recything_application/screens/on_boarding/on_boarding_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,12 +12,32 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    Future.delayed(
+      const Duration(seconds: 5),
+      () {
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const OnBoardingScreen(),
+          ),
+          (route) => false,
+        );
+      },
+    );
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorConstant.netralColor50,
       body: Center(
-        child: Text('Hello World!'),
-      ),
+          child: Image.asset(
+        ImageConstant.logo,
+        height: 50.0,
+        width: 255.0,
+      )),
     );
   }
 }
