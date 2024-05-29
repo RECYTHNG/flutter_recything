1c925582130880dafa91ff9d8425da2e08c732d8
ztzrk <ralif877@gmail.com>
2024-05-30 00:39:43 +0700

    feat: add pick trash screen

--- recything_application/lib/main.dart
+++ recything_application/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:recything_application/constants/app_theme_constant.dart';
 import 'package:recything_application/screens/article/article_screen.dart';
 import 'package:recything_application/screens/edit_profile/edit_profile_screen.dart';
+import 'package:recything_application/screens/report_rubbish/pick_trash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: AppThemeConstant.appTheme,
       debugShowCheckedModeBanner: false,
-      home: const ArticleScreen(),
+      home: const PickTrashScreen(),
     );
   }
 }
