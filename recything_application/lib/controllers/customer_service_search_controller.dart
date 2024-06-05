24ae77d267e7feab9b8da164489e54e14c0b3e55
Fadhl Al-Hafizh <fadhlhafizh18@students.unnes.ac.id>
2024-06-05 18:03:02 +0700

    feat(customer-service-search): adding state management for customer-service-search

---
+++ recything_application/lib/controllers/customer_service_search_controller.dart
@@ -0,0 +1,76 @@
+import 'dart:async';
+
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:recything_application/models/faq/search_faq_model.dart';
+import 'package:recything_application/services/faq_services/search_faq_service.dart';
+
+class CustomerServiceSearchController extends GetxController {
+  final formKey = GlobalKey<FormState>();
+  var searchController = TextEditingController().obs;
+  var searchResults = <DatumSearch>[].obs;
+  var matchData = <String>[].obs;
+  RxString queryInput = ''.obs;
+  var isLoading = false.obs;
+  Timer? _debounce;
+
+  @override
+  void dispose() {
+    searchController.value.dispose();
+    super.dispose();
+  }
+
+  Future<void> searchFaq(String query) async {
+    isLoading.value = true;
+    try {
+      final searchResult = await SearchFaqService().getSearchFaq(query: query);
+      searchResults.value = searchResult.data ?? [];
+      isLoading.value = false;
+    } catch (e) {
+      searchResults.clear();
+      Get.snackbar(
+        '',
+        '',
+        padding: const EdgeInsets.all(0),
+        margin: const EdgeInsets.all(12),
+        snackStyle: SnackStyle.FLOATING,
+        backgroundColor: Colors.transparent,
+        barBlur: 0.0,
+        overlayBlur: 0.0,
+        snackPosition: SnackPosition.BOTTOM,
+        messageText: AwesomeSnackbarContent(
+          title: 'Error',
+          message: 'Message : ${e.toString()}',
+          contentType: ContentType.failure,
+        ),
+      );
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  void onSearchChanged(String query) {
+    if (_debounce?.isActive ?? false) _debounce?.cancel();
+    _debounce = Timer(const Duration(milliseconds: 500), () {
+      searchFaq(query);
+    });
+  }
+
+  void onChangedQuery(String value) {
+    queryInput.value = value;
+    if (value.isEmpty) {
+      matchData.clear();
+    } else {
+      matchData.value = searchResults
+          .where((element) => element.question!.contains(queryInput.value))
+          .map((e) => e.question!)
+          .toList();
+    }
+  }
+
+  void onClickMatchedResult(newValue) {
+    matchData.clear();
+    searchController.value.text = newValue;
+  }
+}
--- recything_application/lib/screens/customer_service/search_result_customer_service/search_result_customer_service_screen.dart
+++ recything_application/lib/screens/customer_service/search_result_customer_service/search_result_customer_service_screen.dart
@@ -1,14 +1,12 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:recything_application/constants/color_constant.dart';
 import 'package:recything_application/constants/spacing_constant.dart';
 import 'package:recything_application/constants/text_style_constant.dart';
-import 'package:recything_application/models/faq/search_faq_model.dart';
+import 'package:recything_application/controllers/customer_service_search_controller.dart';
 import 'package:recything_application/screens/customer_service/detail_answer_faq_or_other/detail_answer_faq_or_other_screen.dart';
 import 'package:recything_application/screens/customer_service/search_result_customer_service/widgets/search_not_found_widget.dart';
 import 'package:recything_application/screens/customer_service/cutomer_service_faq_main/widgets/item_list_faq_widget.dart';
-import 'package:recything_application/services/faq_services/search_faq_service.dart';
 import 'package:recything_application/widgets/global_autocomplete_search_bar.dart';
 import 'package:recything_application/widgets/global_loading_widget.dart';
 
@@ -26,80 +24,21 @@ class SearchResultCustomerService extends StatefulWidget {
 
 class _SearchResultCustomerServiceState
     extends State<SearchResultCustomerService> {
-  final TextEditingController _searchController = TextEditingController();
-  var formKey = GlobalKey<FormState>();
-  List<DatumSearch> searchResults = [];
-  List<String> matchData = [];
-  String? queryInput;
-  bool isLoading = false;
-  Timer? _debounce;
-
-  @override
-  void dispose() {
-    _searchController.dispose();
-    super.dispose();
-  }
-
-  Future<void> _searchFaq(String query) async {
-    setState(() {
-      isLoading = true;
-    });
-
-    try {
-      final searchResult = await SearchFaqService().getSearchFaq(query: query);
-      setState(() {
-        searchResults = searchResult.data ?? [];
-        isLoading = false;
-      });
-    } catch (e) {
-      setState(() {
-        searchResults = [];
-      });
-      SnackBar(
-        content: Text('Error: ${e.toString()}'),
-      );
-    } finally {
-      setState(() {
-        isLoading = false;
-      });
-    }
-  }
-
-  void _onSearchChanged(String query) {
-    if (_debounce?.isActive ?? false) _debounce?.cancel();
-    _debounce = Timer(const Duration(milliseconds: 500), () {
-      _searchFaq(query);
-    });
-  }
-
-  void onChangedQuery(String value) {
-    queryInput = value;
-    setState(() {});
-    if (queryInput == "") {
-      matchData.clear();
-    } else {
-      matchData = searchResults
-          .where((element) => element.question!.contains(queryInput!))
-          .map((e) => e.question!)
-          .toList();
-    }
-    setState(() {});
-  }
-
-  void onClickMatchedResult(newValue) {
-    matchData.clear();
-    _searchController.text = newValue;
-    setState(() {});
-  }
+  final CustomerServiceSearchController customerServiceSearchController =
+      Get.put(
+    CustomerServiceSearchController(),
+  );
 
   @override
   void initState() {
     super.initState();
-    _searchFaq(widget.query);
-    _searchController.addListener(() {
-      _onSearchChanged(_searchController.text);
-    });
-    _searchFaq(widget.query);
+    customerServiceSearchController.searchFaq(widget.query);
+    customerServiceSearchController.searchController.value.addListener(
+      () {
+        customerServiceSearchController.onSearchChanged(
+            customerServiceSearchController.searchController.value.text);
+      },
+    );
   }
 
   @override
@@ -122,52 +61,60 @@ class _SearchResultCustomerServiceState
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            Flexible(
-              child: GlobalAutocompleteSearchBar(
-                height: 48.0,
-                width: double.infinity,
-                controller: _searchController,
-                matchedSearchData: matchData,
-                hintText: 'Search',
-                query: queryInput,
-                onSubmitted: (value) {
-                  _searchFaq(value);
-                },
-                onChanged: (value) {
-                  onChangedQuery(value);
-                },
-                onResultSelected: (newValue) {
-                  onClickMatchedResult(newValue);
-                },
-              ),
-            ),
-            SpacingConstant.verticalSpacing200,
-            if (isLoading) const Center(child: MyLoading()),
-            if (!isLoading && searchResults.isNotEmpty)
-              Expanded(
-                child: ListView.builder(
-                  itemCount: searchResults.length,
-                  itemBuilder: (context, index) {
-                    final faq = searchResults[index];
-                    return ItemListFaqWidget(
-                      question: faq.question ?? '',
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => DetailAnswerFAQorOtherScreen(
-                              question: faq.question ?? '',
-                              answer: faq.answer ?? '',
-                            ),
-                          ),
-                        );
-                      },
-                    );
+            Obx(
+              () => Flexible(
+                child: GlobalAutocompleteSearchBar(
+                  height: 48.0,
+                  width: double.infinity,
+                  controller:
+                      customerServiceSearchController.searchController.value,
+                  matchedSearchData: customerServiceSearchController.matchData,
+                  hintText: 'Search',
+                  query: customerServiceSearchController.queryInput.value,
+                  onSubmitted: (value) {
+                    customerServiceSearchController.searchFaq(value);
+                  },
+                  onChanged: (value) {
+                    customerServiceSearchController.onChangedQuery(value);
+                  },
+                  onResultSelected: (newValue) {
+                    customerServiceSearchController
+                        .onClickMatchedResult(newValue);
                   },
                 ),
               ),
-            if (!isLoading && searchResults.isEmpty)
-              const SearchingNotFoundWidget()
+            ),
+            SpacingConstant.verticalSpacing200,
+            Obx(() {
+              if (customerServiceSearchController.isLoading.value) {
+                return const Center(child: MyLoading());
+              } else if (!customerServiceSearchController.isLoading.value &&
+                  customerServiceSearchController.searchResults.isNotEmpty) {
+                return Expanded(
+                  child: ListView.builder(
+                    itemCount:
+                        customerServiceSearchController.searchResults.length,
+                    itemBuilder: (context, index) {
+                      final faq =
+                          customerServiceSearchController.searchResults[index];
+                      return ItemListFaqWidget(
+                        question: faq.question ?? '',
+                        onTap: () {
+                          Get.to(
+                            () => DetailAnswerFAQorOtherScreen(
+                              question: faq.question ?? '',
+                              answer: faq.answer ?? '',
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  ),
+                );
+              } else {
+                return const SearchingNotFoundWidget();
+              }
+            }),
           ],
         ),
       ),
