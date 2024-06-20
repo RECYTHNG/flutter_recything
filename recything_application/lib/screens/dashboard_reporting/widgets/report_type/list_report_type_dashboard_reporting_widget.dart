240665675cd59dae0e2353c52d3e13bcf03b34fc
ztzrk <ralif877@gmail.com>
2024-06-21 00:14:16 +0800

    fix: navigation

--- recything_application/lib/screens/dashboard_reporting/widgets/report_type/list_report_type_dashboard_reporting_widget.dart
+++ recything_application/lib/screens/dashboard_reporting/widgets/report_type/list_report_type_dashboard_reporting_widget.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:recything_application/constants/image_constant.dart';
 import 'package:recything_application/controllers/report_history_controller.dart';
 import 'package:recything_application/screens/dashboard_reporting/widgets/report_type/item_report_type_dashboard_reporting_widget.dart';
+import 'package:recything_application/screens/report_litter/pick_litter_type_screen.dart';
 import 'package:recything_application/screens/report_rubbish/report_rubbish_screen.dart';
 
 class ListReportTypeDashboardReportingWidget extends StatelessWidget {
@@ -17,7 +18,7 @@ class ListReportTypeDashboardReportingWidget extends StatelessWidget {
     ),
     ItemReportTypeDashboardReportingWidget(
       onTap: () {
-        Get.to(const ReportRubbishScreen());
+        Get.to(const PickLitterTypeScreen());
       },
       title: 'Pembuangan Sampah Sembarangan',
       image: ImageConstant.litteringCarousel,
--- recything_application/lib/screens/report_litter/success_report_litter_screen.dart
+++ recything_application/lib/screens/report_litter/success_report_litter_screen.dart
@@ -7,7 +7,9 @@ import 'package:recything_application/constants/spacing_constant.dart';
 import 'package:recything_application/constants/text_style_constant.dart';
 import 'package:recything_application/controllers/report_litter_controller.dart';
 import 'package:recything_application/controllers/map_litter_controller.dart';
+import 'package:recything_application/screens/dashboard_reporting/dashboard_reporting_screen.dart';
 import 'package:recything_application/screens/home.dart';
+import 'package:recything_application/screens/homepage/homepage_screen.dart';
 import 'package:recything_application/utils/system_ui_overlay_utils.dart';
 import 'package:recything_application/widgets/global_app_bar.dart';
 import 'package:recything_application/widgets/global_button_widget.dart';
@@ -72,7 +74,7 @@ class SuccessReportLitterScreen extends StatelessWidget {
                 Get.delete<MapLitterController>();
                 Get.delete<ReportLitterController>();
                 Get.to(
-                  () => const HomeAuthenticationScreen(),
+                  () => const DashboardReportingScreen(),
                 );
               },
               width: double.infinity,
